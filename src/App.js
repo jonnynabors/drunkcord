ba37e27e4e1d6a54148e2d796520276cbdbea0a0
Jonny Nabors <jonnynabors@gmail.com>
2019-05-10 20:58:39 -0400

    Add initial styling with css grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="container">
+      <header className="header">
+        <div>
+          <h1 style={{ display: "inline" }}>Drunkcord</h1>
+          <h3 style={{ display: "inline" }}>
+            A Discord Bot to keep track of who is drinking in the Discord
+            Server!
+          </h3>
+        </div>
+      </header>
+      <div className="main">
+        The best bot for getting drunk with your friends
+      </div>
+      <div className="footer">
+        Made with{" "}
+        <span role="img" aria-label="heart">
+          ❤️
+        </span>
+        ️ by
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/jonnynabors"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jonny Nabors
         </a>
-      </header>
+      </div>
     </div>
   );
 }
