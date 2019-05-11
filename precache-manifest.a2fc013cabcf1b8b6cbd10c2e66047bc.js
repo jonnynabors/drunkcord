a9e3c00243125460c52183e48dfc92cb1379a4bb
Jonny Nabors <jonnynabors@gmail.com>
2019-05-10 20:10:36 -0400

    Updates

---
+++ precache-manifest.a2fc013cabcf1b8b6cbd10c2e66047bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765a11006e2d8cc747889d65f2c98dcd",
+    "url": "/drunkcord/index.html"
+  },
+  {
+    "revision": "eacb3c8487f08e6a4423",
+    "url": "/drunkcord/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/drunkcord/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "eacb3c8487f08e6a4423",
+    "url": "/drunkcord/static/js/main.68f32626.chunk.js"
+  },
+  {
+    "revision": "aa22d4f98022e8843a62",
+    "url": "/drunkcord/static/js/runtime~main.10b7f1ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drunkcord/static/media/logo.5d5d9eef.svg"
+  }
+]);
